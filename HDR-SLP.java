4d55f41217ad512c7d1347eb0ec46a88f6c96663
Jaimi Chong <jaimi.chong@gmail.com>
2023-11-11 16:18:22 -0800

    rename file, imports
    HDR-SLP wasn't a valid class name.

--- HDR-SLP.java
+++
@@ -1,2 +0,0 @@
-// HDR-SLP.java - HDR Stagelist Previewer GUI
-// Written by Jaimi, last edit in Nov. 2023
---
+++ HDR_SLP.java
@@ -0,0 +1,17 @@
+// HDR-SLP.java - HDR Stagelist Previewer GUI
+// Written by JaiChong "ECHOnce"
+// Last edited in November 2023
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+import javax.swing.*;
+
+
+public class HDR_SLP extends JFrame {
+    
+}
